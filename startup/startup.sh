37427f4c92b7a7399948ca8c6cb2bd5f15053e4b
Adam Callin <adam.callin@callin.ca>
2024-06-09 18:46:04 -0400

    Fix typo

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-screen -dmS mc-home-server-minecraft-server -c 'cd /usr/local/sbin/mc-home-server/minecraft; java -Xmx4G -Xms4G -jar server.jar nogui'
+screen -dmS mc-home-server-minecraft-server bash -c 'cd /usr/local/sbin/mc-home-server/minecraft; java -Xmx4G -Xms4G -jar server.jar nogui'
 screen -dmS mc-home-server-heartbeat bash -c 'cd /usr/local/sbin/mc-home-server/heartbeat; ./heartbeat.out'
