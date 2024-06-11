fd5a0d647c414d69985d575d9dc66ddd205fce71
Adam Callin <adam.callin@callin.ca>
2024-06-10 20:48:03 -0400

    Add screen blank option in startup script

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#TERM=linux setterm -blank 1 >/dev/tty1 # Uncomment if the server is on a laptop
 screen -dmS minecraft bash -c 'cd /usr/local/sbin/mc-home-server/minecraft; java -Xmx4G -Xms4G -jar server.jar nogui'
 screen -dmS heartbeat bash -c 'cd /usr/local/sbin/mc-home-server/heartbeat; ./heartbeat.out'
 screen -dmS restart bash -c 'cd /usr/local/sbin/mc-home-server/restart; ./restart.out'
