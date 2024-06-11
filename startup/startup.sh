0cceb4fa554bfac237885b9403f2aa3620ade2cb
Adam Callin <52753086+aCallin@users.noreply.github.com>
2024-06-10 20:50:37 -0400

    Update startup.sh

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#TERM=linux setterm -blank 1 >/dev/tty1 # Uncomment if the server is on a laptop
+#TERM=linux setterm -blank 1 >/dev/tty1 # Uncomment to turn off screen after an amount of time
 screen -dmS minecraft bash -c 'cd /usr/local/sbin/mc-home-server/minecraft; java -Xmx4G -Xms4G -jar server.jar nogui'
 screen -dmS heartbeat bash -c 'cd /usr/local/sbin/mc-home-server/heartbeat; ./heartbeat.out'
 screen -dmS restart bash -c 'cd /usr/local/sbin/mc-home-server/restart; ./restart.out'
